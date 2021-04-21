9ec0d8803e44282ac4a1c01708372ca0896a4d8b
qza012 <qza012@naver.com>
2021-04-22 00:27:34 +0900

    문제 풀이
    2752 세 수 정렬

---
+++ baekjoon/2752 세 수 정렬.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*; 
+import java.math.*;
+
+public class Main { 
+	public static void main(String[] args) throws IOException { 
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(System.out);
+		StringTokenizer in = new StringTokenizer(reader.readLine());
+		
+		int[] nums = new int[3];
+		int count = 0;
+		while(in.hasMoreTokens()) {
+			nums[count++] = Integer.parseInt(in.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		
+		for(int num : nums) {
+			out.print(num + " ");
+		}
+		out.flush();
+	} 
+}
