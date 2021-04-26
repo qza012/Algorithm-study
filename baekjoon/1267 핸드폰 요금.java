5c7dffbbd5242294dfa5471feef3dad24576c162
DongJun Yang <qza012@naver.com>
2021-04-26 23:29:58 +0900

    Create 1267 핸드폰 요금.java

---
+++ baekjoon/1267 핸드폰 요금.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*; 
+import java.math.*;
+
+public class Main { 
+	public static void main(String[] args) throws IOException { 
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(System.out);
+        
+        int callCount = Integer.parseInt(reader.readLine());
+		    StringTokenizer in = new StringTokenizer(reader.readLine());
+		
+        int YBill = 0;
+        int MBill = 0;
+        
+        for(int i = 0; i < callCount; ++i ){
+            int callTime = Integer.parseInt(in.nextToken());
+            YBill += (callTime / 30 + 1) * 10;
+            MBill += (callTime / 60 + 1) * 15;
+        }
+        
+        String result = "Y M " + YBill;
+        if(YBill > MBill) {
+            result = "M " + MBill;
+        } else if(YBill < MBill) {
+            result = "Y " + YBill;
+        }
+        
+        out.print(result);
+        out.flush();
+	} 
+}
